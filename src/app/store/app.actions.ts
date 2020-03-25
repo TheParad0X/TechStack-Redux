dd07193770fc5ebe2b1ae5885905c73051da5d64
Tim Gündel <tim.guendel@evodion.de>
2020-03-25 19:44:08 +0100

    WIP

--- src/app/store/app.actions.ts
+++ src/app/store/app.actions.ts
@@ -1,5 +1,8 @@
 import { createAction, props } from '@ngrx/store';
 
+export const DoIncrement = createAction('[App] Do Increment Counter');
+export const DoDecrement = createAction('[App] Do Decrement Counter');
+
 export const DoSetCurrentUser = createAction('[App] Do Set Current User', props<{ user: string }>());
 export const DoAddUser = createAction('[App] Do Add  User', props<{ user: string }>());
 export const DoDeleteUser = createAction('[App] Do Delete User', props<{ user: string }>());
--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -2,12 +2,14 @@ import { createReducer, on } from '@ngrx/store';
 import * as appActions from './app.actions';
 
 export interface AppState {
+  counter: number;
   currentUser: string;
   users: string[];
   loading: boolean;
 }
 
 const initialState: AppState = {
+  counter: 0,
   currentUser: '',
   users: [],
   loading: false
@@ -15,6 +17,10 @@ const initialState: AppState = {
 
 export const appReducer = createReducer(
   initialState,
+
+  on(appActions.DoIncrement, (state) => ({ ...state, counter: state.counter + 1 })),
+  on(appActions.DoIncrement, (state) => ({ ...state, counter: state.counter - 1 })),
+
   //on(appActions.DoSetCurrentUser, (state, { user }) => ({ ...state, currentUser: user })),
   on(appActions.DoSetCurrentUser, (state, { user }) => {
     return { ...state, currentUser: user };
