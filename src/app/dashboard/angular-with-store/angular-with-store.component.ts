0dc63013bc02f6e40f365438f37d6a9f7fe015b8
Tim Gündel <tim.guendel@evodion.de>
2020-03-25 22:13:33 +0100

    WIP

---
+++ src/app/dashboard/angular-with-store/angular-with-store.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-with-store',
+  templateUrl: './angular-with-store.component.html',
+  styleUrls: ['./angular-with-store.component.css']
+})
+export class AngularWithStoreComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/dashboard/angular-with-store/container-with-store/container-with-store.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-container-with-store',
+  templateUrl: './container-with-store.component.html',
+  styleUrls: ['./container-with-store.component.css']
+})
+export class ContainerWithStoreComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
